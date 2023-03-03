35f0b333a24b1f79708af6d1687989c024bf66d3
Hyexr <85266811+Hyexr@users.noreply.github.com>
2023-03-03 21:44:58 +0800

    Create ComboLocks.java

---
+++ ComboLocks.java
@@ -0,0 +1,18 @@
+//32.08 points (34%)
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        System.out.println("scpabmtrdf");
+        System.out.println("aeoirulhnt");
+        System.out.println("ranteilso ");
+        System.out.println("eitanlsrd ");
+        System.out.println("eistrnalo ");
+        System.out.println("ensritadl ");
+        System.out.println("esntrgdal ");
+        System.out.println("sedgynrtl ");
+    }
+}
