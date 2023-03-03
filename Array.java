7e0284fe5bc7594be242c652d1a23effb6c50809
Hyexr <85266811+Hyexr@users.noreply.github.com>
2023-03-03 21:43:37 +0800

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,15 @@
+//18.67 points (20%)
+//骗分
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Please name your class Main
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+	    Scanner in = new Scanner(System.in);
+	    //找不到，输出none
+    	System.out.println("None");
+	}
+}
